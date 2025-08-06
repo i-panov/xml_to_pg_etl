a429c2d63d48cbe5da732e52dadaa2be5df3eadd
i.panov <panov210@yandex.ru>
2025-08-06 15:09:17 +0300

    db

--- src/main/kotlin/db.kt
+++ src/main/kotlin/db.kt
@@ -2,10 +2,8 @@ package ru.my
 
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
-import java.sql.Connection
-import java.sql.DatabaseMetaData
-import java.sql.PreparedStatement
-import java.sql.ResultSet
+import java.math.BigDecimal
+import java.sql.*
 
 data class ColumnInfo(
     val name: String,
@@ -13,7 +11,7 @@ data class ColumnInfo(
     val isNullable: Boolean
 ) {
     constructor(rs: ResultSet): this(
-        name = rs.getString("COLUMN_NAME").lowercase(),
+        name = rs.getString("COLUMN_NAME"),
         type = rs.getInt("DATA_TYPE"),
         isNullable = rs.getInt("NULLABLE") == DatabaseMetaData.columnNullable,
     )
@@ -44,15 +42,57 @@ inline fun <T> Connection.withTransaction(block: () -> T): T {
 }
 
 fun PreparedStatement.setParameter(index: Int, col: ColumnInfo, value: String?) {
-    if (value.isNullOrEmpty()) {
+    if (value == null) {
         if (col.isNullable) {
-            return setNull(index, col.type)
+            setNull(index, col.type)
         } else {
-            throw IllegalStateException("NOT NULL column '${col.name}' is empty")
+            throw IllegalStateException("NOT NULL column '${col.name}' cannot be null")
+        }
+    } else {
+        when (col.type) {
+            Types.CHAR, Types.VARCHAR, Types.LONGVARCHAR,
+            Types.NCHAR, Types.NVARCHAR, Types.LONGNVARCHAR,
+            Types.CLOB, Types.NCLOB, Types.ROWID -> setString(index, value)
+
+            Types.BIT, Types.BOOLEAN -> setBoolean(index, value.toBoolean())
+
+            Types.TINYINT -> setByte(index, value.toByte())
+            Types.SMALLINT -> setShort(index, value.toShort())
+            Types.INTEGER -> setInt(index, value.toInt())
+            Types.BIGINT -> setLong(index, value.toLong())
+
+            Types.FLOAT, Types.REAL -> setFloat(index, value.toFloat())
+            Types.DOUBLE -> setDouble(index, value.toDouble())
+
+            Types.NUMERIC, Types.DECIMAL -> setBigDecimal(index, BigDecimal(value))
+
+            Types.DATE -> setDate(index, Date.valueOf(value))
+            Types.TIME -> setTime(index, Time.valueOf(value))
+
+            Types.TIMESTAMP -> {
+                val timestampValue = if (value.matches(Regex("\\d{4}-\\d{2}-\\d{2}"))) {
+                    "$value 00:00:00" // Для Timestamp.valueOf важно чтобы время было указано
+                } else {
+                    value
+                }
+                setTimestamp(index, Timestamp.valueOf(timestampValue))
+            }
+
+            Types.BINARY, Types.VARBINARY, Types.LONGVARBINARY, Types.BLOB -> setBytes(index, hexToBytes(value))
+
+            Types.OTHER, Types.JAVA_OBJECT, Types.DISTINCT, Types.STRUCT,
+            Types.ARRAY, Types.REF, Types.DATALINK, Types.SQLXML,
+            Types.REF_CURSOR, Types.TIME_WITH_TIMEZONE, Types.TIMESTAMP_WITH_TIMEZONE ->
+                throw UnsupportedOperationException("Type ${col.type} not supported")
+
+            else -> throw IllegalArgumentException("Unknown type: ${col.type}")
         }
     }
+}
 
-    return setString(index, value)
+fun hexToBytes(hex: String): ByteArray {
+    require(hex.length % 2 == 0) { "Hex string must have even length" }
+    return hex.chunked(2).map { it.toInt(16).toByte() }.toByteArray()
 }
 
 fun createDataSource(dbConfig: DbConfig): HikariDataSource {
